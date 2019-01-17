2cd8d75110dae0daecf4b939342764f3949a193b
Eduard Shavyrko <soc_35m@protonmail.com>
2019-01-17 18:57:36 +0200

    add isConnected() method which implements Breadth-First Search

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+// Queue for Breadth-First Search
+class Queue {
+    constructor() {
+        this.data = [];
+    }
+    add(node) {
+        this.data.push(node);
+    }
+    get() {
+        return this.data.shift();
+    }
+    isEmpty() {
+        return !Boolean(this.data.length);
+    }
+}
+
 class Graph {
     constructor(arr = []) {
         this.noOfNodes = arr.length;
@@ -12,6 +28,7 @@ class Graph {
             return true;
         }
     }
+    // undirected graph
     addEdge(src, dest) {
         if (this.adjSet.get(src).has(dest)) {
             return false;
@@ -30,9 +47,44 @@ class Graph {
             console.log(str);
         });
     }
+    // Breadth-First Search
+    isConnected(src, dest) {
+        const queue = new Queue();
+        queue.add(src);
+        const marked = new Set();
+        marked.add(src);
+        while (!queue.isEmpty()) {
+            const current = queue.get();
+            console.log(current);
+            if (current === dest) return true;
+            this.adjSet.get(current).forEach(value => {
+                if (!marked.has(value)) {
+                    queue.add(value);
+                    marked.add(value);
+                }
+            });
+        }
+        return false;
+    }
 }
 
-const g = new Graph(['Chicago','Boston','New York','Los Angeles','Philadelphia','Detroit']);
-g.addEdge('Chicago','Boston');
+const g = new Graph(['Detroit','Chicago','Philadelphia','Boston','New York']);
+
+g.addNode('San Francisco');
+
+g.addEdge('Detroit','Chicago');
+g.addEdge('Detroit','Philadelphia');
 g.addEdge('Chicago','New York');
+g.addEdge('Chicago','Boston');
+g.addEdge('Philadelphia','Boston');
+g.addEdge('Philadelphia','San Francisco');
+g.addEdge('Philadelphia','New York');
+g.addEdge('Boston','New York');
+g.addEdge('New York','San Francisco');
+
 g.printGraph();
+
+console.log(g.isConnected('Detroit', 'Philadelphia'));
+console.log(g.isConnected('Detroit', 'San Francisco'));
+g.addNode('Los Angeles');
+console.log(g.isConnected('Detroit', 'Los Angeles'));
